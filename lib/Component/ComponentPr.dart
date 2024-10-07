94f20fa9e8b71ced33ab8c7d12fe68154985ef63
SiPalingMumet <moch_ardhiansah_31rpl@student.smktelkom-mlg.sch.id>
2024-10-07 14:03:50 +0700

    finishing 1

--- lib/Component/ComponentPr.dart
+++ lib/Component/ComponentPr.dart
@@ -5,51 +5,53 @@ class Widgetz extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(0),
       child: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Column(
               children: [
-                Row(
-                  children: [
-                    const CircleAvatar(
-                      backgroundImage: AssetImage('images/tole.png'),
-                      radius: 24,
-                    ),
-                    const SizedBox(width: 5),
-                    Expanded(
-                      child: TextButton(
-                        onPressed: () {
-                          // funtion.tanyaSoal(context, setState);
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 20),
-                          decoration: BoxDecoration(
-                            color: Colors.grey[200],
-                            borderRadius: BorderRadius.circular(8),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(
-                                    0.1), 
-                                spreadRadius: 3, 
-                                blurRadius: 6, 
-                                offset: const Offset(0,
-                                    3), 
-                              ),
-                            ],
-                          ),
-                          child: Text(
-                            'Soal apa yang ingin kamu tanyain?                 ',
-                            style: GoogleFonts.actor(
-                                fontSize: 12, color: Colors.grey),
+                Padding(
+                  padding: const EdgeInsets.all(0),
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 20),
+                      const CircleAvatar(
+                        backgroundImage: AssetImage('images/tole.png'),
+                        radius: 24,
+                      ),
+                      const SizedBox(width: 4),
+                      Expanded(
+                        child: TextButton(
+                          onPressed: () {
+                            // funtion.tanyaSoal(context, setState);
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 20),
+                            decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(8),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.1),
+                                  spreadRadius: 3,
+                                  blurRadius: 6,
+                                  offset: const Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Text(
+                              'Soal apa yang ingin kamu tanyain?            ',
+                              style: GoogleFonts.actor(
+                                  fontSize: 12, color: Colors.grey),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -57,26 +59,28 @@ class Widgetz extends StatelessWidget {
                     Row(
                       children: [
                         Padding(
-                          padding: const EdgeInsets.only(left: 60),
+                          padding: const EdgeInsets.only(left: 87),
                           child: IconButton(
-                            icon: const Icon(Icons.photo_camera,
-                                color: Colors.grey, size: 20,),
+                            icon: const Icon(
+                              Icons.photo_camera,
+                              color: Colors.grey,
+                              size: 20,
+                            ),
                             onPressed: () {
                               // funtion.tanyaSoal(context, setState);
                             },
                           ),
                         ),
-                        const SizedBox(
-                            width: 2), 
+                        const SizedBox(width: 2),
                         Text(
                           "Foto Soal",
                           style: GoogleFonts.actor(
-                              fontSize: 10, color: Colors.grey), 
+                              fontSize: 10, color: Colors.grey),
                         ),
                       ],
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(right: 20),
+                      padding: const EdgeInsets.only(right: 30),
                       child: ElevatedButton(
                         onPressed: () {
                           // funtion.tanyaSoal(context, setState);
@@ -89,7 +93,7 @@ class Widgetz extends StatelessWidget {
                             borderRadius: BorderRadius.circular(8),
                           ),
                           elevation: 6,
-                          minimumSize: const Size(0, 30), 
+                          minimumSize: const Size(0, 30),
                         ),
                         child: Text(
                           '  Upload  ',
@@ -99,10 +103,21 @@ class Widgetz extends StatelessWidget {
                       ),
                     ),
                   ],
-                )
+                ),
               ],
             ),
-            
+            const SizedBox(height: 5),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              color: const Color.fromARGB(255, 235, 235, 235),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  '  Filter berdasarkan :',
+                  style: GoogleFonts.actor(fontSize: 14),
+                ),
+              ),
+            ),
           ],
         ),
       ),
